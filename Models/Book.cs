13254b83b18084dc9fe98d51bf7eec90a23d47c6
JustCode09 <Professionalacc011@gmail.com>
2026-05-30 17:45:50 +0545

    Added AppDbContext with Books, Members, Borrowings tables

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,12 @@
 ﻿namespace LibraryAPI.Models
+
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? ISBN { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
--- Models/Borrowing.cs
+++ Models/Borrowing.cs
@@ -2,5 +2,10 @@
 {
     public class Borrowing
     {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int BookId { get; set; }
+        public DateTime BorrowedDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
