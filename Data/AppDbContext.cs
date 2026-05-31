0ef06ecea100b5fdd06286ffc75e17b6a8b464b7
JustCode09 <Professionalacc011@gmail.com>
2026-05-31 12:19:48 +0545

    Added connection string for LibraryDB

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace LibraryAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Borrowing> Borrowings { get; set; }
     }
 }
